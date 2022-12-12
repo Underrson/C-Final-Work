56538f88d85ddfc4f349387588eb1ed6b06165f4
Виктория Тонявина <viktoriatonavina@MacBook-Air-Viktoria.local>
2022-12-12 19:29:49 +0300

    add the PrintArray  void function

--- Code/TaskCode/Program.cs
+++ Code/TaskCode/Program.cs
@@ -1,11 +1,10 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
 
-string[] array1 = new string[7] {"278", "75", "hello", "array", "repository", "func", "recursion"};
+string[] array1 = new string[7] {"278", "75", "hello", "array", "rep", "func", "recursion"};
 string[] array2 = new string[array1.Length];
-
+int count = 0;
 void SecondArrayWithIF(string[] array1, string[] array2)
 {
-    int count = 0;
     for (int i = 0; i < array1.Length; i++)
     {
     if(array1[i].Length <= 3)
@@ -15,3 +14,14 @@ void SecondArrayWithIF(string[] array1, string[] array2)
         }
     }
 }
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
