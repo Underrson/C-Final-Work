3a5f3f08f71df31cc9c201216220268432bc4c5e
Виктория Тонявина <viktoriatonavina@MacBook-Air-Viktoria.local>
2022-12-12 19:27:16 +0300

    add the SecondArray void function

--- Code/TaskCode/Program.cs
+++ Code/TaskCode/Program.cs
@@ -2,4 +2,16 @@
 
 string[] array1 = new string[7] {"278", "75", "hello", "array", "repository", "func", "recursion"};
 string[] array2 = new string[array1.Length];
-int count = 0;
+
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
