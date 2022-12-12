1e13f545ce42f61a9a4fe1d70b05b533f5e64380
Виктория Тонявина <viktoriatonavina@MacBook-Air-Viktoria.local>
2022-12-12 19:24:01 +0300

    add the array and count

---
+++ Code/TaskCode/Program.cs
@@ -0,0 +1,5 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
+
+string[] array1 = new string[7] {"278", "75", "hello", "array", "repository", "func", "recursion"};
+string[] array2 = new string[array1.Length];
+int count = 0;
